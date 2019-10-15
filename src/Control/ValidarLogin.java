72f3d41ad86f1cc186b26eeecffe09a5c14c29e3
Estudiante <Estudiante@BE454S403C33ING.unal.loc>
2019-10-15 16:04:31 -0500

    Lab4 falta 3 parte

--- src/Control/ValidarLogin.java
+++ src/Control/ValidarLogin.java
@@ -1,14 +1,15 @@
 
 package Control;
 
-import Entidad.Sistema;
+//import Entidad.Sistema;
+import DAO.UsuarioDAO;
 import Entidad.Usuario;
-import Frontera.FramePrincipal;
+//import Frontera.FramePrincipal;
 
 public class ValidarLogin {
     
-    private Sistema sistema = FramePrincipal.sistema;
-    
+    //private Sistema sistema = FramePrincipal.sistema;
+    private UsuarioDAO dao = new UsuarioDAO();
     public ValidarLogin(){    
     }
     
@@ -20,11 +21,14 @@ public class ValidarLogin {
         if(!verificarLongitudPassword(usuario.getPassword())){
             return("Longitud contraseña incorrecta");
         }
-        for(Usuario u: sistema.getUsuarios()){
+        /*for(Usuario u: sistema.getUsuarios()){
             if(u.getNombre().equals(usuario.getNombre())&&
                u.getPassword().equals(usuario.getPassword())){
                 return ("Bienvenido");
             }
+        }*/
+        if(dao.leer(usuario)!=null){
+            return("Bienvenido");
         }
         return ("Datos incorrectos");
     }
